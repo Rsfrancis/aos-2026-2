9d52ac18aa2b5d647d0e14e8c7e11c45d17df77b
Rsfrancis <arturfdamas2@gmail.com>
2026-04-21 17:35:57 +0000

    fix vercel

--- express-01/api/index.js
+++ express-01/api/index.js
@@ -6,17 +6,20 @@ import models, { sequelize } from "./models";
 import routes from "./routes";
 
 const app = express();
+
 app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: await models.User.findByLogin("rwieruch"),
+    me: null,
   };
   next();
 });
+
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
@@ -28,62 +31,16 @@ app.use("/messages", routes.message);
 app.use("/tarefas", routes.tarefa);
 
 app.get("/", (req, res) => {
-  res.send(
-    "Received a GET HTTP method\nServidor rodando!\n" + process.env.MESSAGE,
-  );
+  res.send("API rodando 🚀");
 });
 
-const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC === "true";
-
-
-const createUsersWithMessages = async () => {
-  await models.User.create(
-    {
-      username: "rwieruch",
-      email: "rwieruch@email.com",
-      messages: [
-        {
-          text: "Published the Road to learn React",
-        },
-      ],
-    },
-    {
-      include: [models.Message],
-    },
-  );
-
-  await models.User.create(
-    {
-      username: "ddavids",
-      email: "ddavids@email.com",
-      messages: [
-        {
-          text: "Happy to release ...",
-        },
-        {
-          text: "Published a complete ...",
-        },
-      ],
-    },
-    {
-      include: [models.Message],
-    },
-  );
-};
+sequelize.sync().catch(console.error);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   const status = err.status || 500;
-  const message = err.message || 'Erro interno do servidor';
+  const message = err.message || "Erro interno do servidor";
   res.status(status).json({ error: message });
-});
-
-sequelize.sync();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
 });
 
 export default app;
